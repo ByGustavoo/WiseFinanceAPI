737aec1b4e05efcfd733e5b2a7a1a8d75672554d
ByGustavoo <gustavo.ccx@hotmail.com>
2025-11-01 14:58:08 -0300

    - Commit de Inicialização

---
+++ src/main/java/br/com/wisefinance/api/WiseFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wisefinance.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseFinanceApiApplication.class, args);
+	}
+
+}
